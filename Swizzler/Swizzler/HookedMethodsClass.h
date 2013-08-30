23928817401897b68d152286b10b071a1ca667af
Ryan Welton <Ryan.G.Welton@gmail.com>
2013-08-30 13:24:42 -0400

    Updates

---
+++ Swizzler/Swizzler/HookedMethodsClass.h
@@ -0,0 +1,13 @@
+//
+//  HookedMethodsClass.h
+//  Swizzler
+//
+//  Created by Ryan on 8/28/13.
+//  Copyright (c) 2013 Ryan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HookedMethodsClass : NSObject
+
+@end
---
+++ Swizzler/Swizzler/HookedMethodsClass.m
@@ -0,0 +1,13 @@
+//
+//  HookedMethodsClass.m
+//  Swizzler
+//
+//  Created by Ryan on 8/28/13.
+//  Copyright (c) 2013 Ryan. All rights reserved.
+//
+
+#import "HookedMethodsClass.h"
+
+@implementation HookedMethodsClass
+
+@end
